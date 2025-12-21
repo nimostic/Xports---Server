14f1e0534531e9b495bcf5bfca7270e66098d2ce
Abu Nayeem Riyad <rionimo5@gmail.com>
2025-12-21 18:32:25 +0600

    Remove node_modules and add gitignore

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gubl8vg.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const myDB = client.db("xports");
+    const userCollection = myDB.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+      const result = userCollection.insertOne(user);
+      res.send(result);
+      console.log(user);
+    });
+
+    app.get("/users", async (req, res) => {
+      const cursor = await userCollection.find().toArray();
+      res.send(cursor);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
