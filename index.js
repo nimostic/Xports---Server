de2c37c48871f32945be9952eb4bab5a42e65711
Abu Nayeem Riyad <rionimo5@gmail.com>
2025-12-24 20:59:02 +0600

    payment api done except duplication

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
     const userCollection = myDB.collection("users");
     const contestCollection = myDB.collection("contests");
     const submissionsCollection = myDB.collection("submissions");
-    const paymentsCollection = myDB.collection("payments");
+    // const paymentsCollection = myDB.collection("payments");
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -38,7 +38,7 @@ async function run() {
       }
       const result = userCollection.insertOne(user);
       res.send(result);
-      console.log(user);
+      // console.log(user);
     });
 
     app.get("/users", async (req, res) => {
@@ -48,7 +48,7 @@ async function run() {
 
     app.post("/contests", async (req, res) => {
       const data = req.body;
-      console.log(data);
+      // console.log(data);
       const result = await contestCollection.insertOne(data);
       res.send(result);
     });
@@ -85,82 +85,171 @@ async function run() {
     //submissions task
     app.post("/submissions/task", async (req, res) => {
       const data = req.body;
-      const { participantEmail, contestId } = req.body;
+      // console.log(data);
+      const { participantEmail, contestId, submissionLink, submittedAt } =
+        req.body;
       let query = {
         participantEmail: participantEmail,
         contestId: contestId,
       };
-      const alreadySubmitted = await submissionsCollection.findOne(query);
-      if (alreadySubmitted) {
+      const submissions = await submissionsCollection.findOne(query);
+      // console.log({submissionsCollection : submissions});
+      if (!submissions) {
+        return res.status(404).send({
+          message: "Registration not found for this contest",
+        });
+      }
+      if (submissions.submissionStatus === "submitted") {
         return res.status(400).send({
           message: "You have already submitted for this contest",
           registered: true,
         });
       }
-      console.log(data);
-      const result = await submissionsCollection.insertOne(data);
+
+      const result = await submissionsCollection.updateOne(
+        {
+          _id: new ObjectId(submissions._id),
+        },
+        {
+          $set: {
+            submissionLink: submissionLink,
+            submissionStatus: "submitted",
+            submittedAt: submittedAt,
+          },
+        },
+        { upsert: true }
+      );
       res.send(result);
     });
 
     app.get("/submissions/check", async (req, res) => {
       const { email, contestId } = req.query;
+      // console.log(contestId);
+      if (!email || !contestId) {
+        return res.status(400).send({ message: "Missing params" });
+      }
+
       let query = {
         participantEmail: email,
         contestId: contestId,
+        paymentStatus: "paid",
       };
-      const paymentExists = await paymentsCollection.findOne(query);
-      if (paymentExists) {
-        return res.send({ registered: true });
+      const submission = await submissionsCollection.findOne(query);
+      if (submission) {
+        return res.send({
+          registered: true,
+          status: submission.submissionStatus,
+        });
       }
       res.send({ registered: false });
     });
 
-
     //===================payment related api====================
 
     app.post("/create-checkout-session", async (req, res) => {
-      const submitInfo = req.body
-      const amount = parseInt(submitInfo.price) * 100
+      const submitInfo = req.body;
+      console.log(submitInfo);
+      const amount = parseInt(submitInfo.price) * 100;
 
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-            price_data : {
-              currency : 'usd',
+            price_data: {
+              currency: "usd",
               unit_amount: amount,
-              product_data : {
-                name : `Please pay for : ${submitInfo.contestName}`
-              }
+
+              product_data: {
+                name: `Please pay for : ${submitInfo.contestName}`,
+                description: submitInfo?.description,
+                images: [submitInfo?.image],
+              },
             },
             quantity: 1,
           },
         ],
         mode: "payment",
-        metadata:{
-          contestId : submitInfo.contestId
+        metadata: {
+          contestId: submitInfo.contestId,
+          participantName: submitInfo.participantName,
+          participantPhoto: submitInfo.participantPhoto,
         },
-        customer_email : submitInfo.participantEmail,
+        customer_email: submitInfo.participantEmail,
         success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled/${submitInfo.contestId}`,
       });
 
-      res.send({url : session.url});
+      res.send({ url: session.url });
     });
 
-    app.get("/checkout-session/:sessionId",async(req,res)=>{
-      try{
-        const session = await stripe.checkout.sessions.retrieve(req.params.sessionId)
+    app.get("/checkout-session/:sessionId", async (req, res) => {
+      try {
+        const session = await stripe.checkout.sessions.retrieve(
+          req.params.sessionId
+        );
         res.send({
-          transactionId:session.payment_intent,
-          contestId : session.metadata.contestId,
-          email : session.customer_email,
-          paymentStatus : session.payment_status,
-          amount : session.amount_total,
-        })
-      }catch(error){
-        res.status(400).send({error:error.message})
+          transactionId: session.payment_intent,
+          contestId: session.metadata.contestId,
+          email: session.customer_email,
+          paymentStatus: session.payment_status,
+          amount: session.amount_total,
+        });
+      } catch (error) {
+        res.status(400).send({ error: error.message });
       }
-    })
+    });
+
+    app.post("/payment-success", async (req, res) => {
+      const { sessionId } = req.body;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+      const existingPayment = await submissionsCollection.findOne({
+        transactionId: session.payment_intent,
+      });
+      if (existingPayment) {
+        return res.send({ success: true, message: "Already processed" });
+      }
+
+      const {
+        contestId,
+        contestName,
+        contestType,
+        prizeMoney,
+        bannerImage,
+        ownerEmail,
+      } = await contestCollection.findOne({
+        _id: new ObjectId(session.metadata.contestId),
+      });
+      // console.log(session);
+      const submitInfo = {
+        contestName: contestName,
+        contestId: session.metadata.contestId,
+        contestType: contestType,
+        bannerImage: bannerImage,
+        ownerEmail: ownerEmail,
+        participantEmail: session.customer_email,
+        participantName: session.metadata.participantName,
+        participantPhoto: session.metadata.participantPhoto,
+        paymentStatus: session.payment_status,
+        transactionId: session.payment_intent,
+        submissionStatus: "pending",
+        prizeMoney: prizeMoney,
+        price: session.amount_total / 100,
+        paidAt: new Date(),
+      };
+      const result = await submissionsCollection.insertOne(submitInfo);
+
+      // update participations in contest
+      await contestCollection.updateOne(
+        {
+          _id: new ObjectId(session.metadata.contestId),
+        },
+        { $inc: { participantsCount: 1 } }
+      );
+
+      return res.send({
+        success: true,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
